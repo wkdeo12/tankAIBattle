f9d81714ecc6c2eef9b2a29245d54df4d77987a1
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-04 21:58:01 +0900

    탱크프리팹 수정
    수정

---
+++ TankAIBattle/Assets/TankAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD
+{
+    public class TankAI : MonoBehaviour
+    {
+        private Rigidbody rb;
+        private Transform headTransform;
+        private Transform Po;
+        public GameObject bullet;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            headTransform = transform.GetChild(1).transform;
+            Po = headTransform.GetChild(0).GetChild(0);
+            headTransform.rotation = Quaternion.EulerRotation(0, 45, 0);
+            Instantiate(bullet, Po);
+        }
+    }
+}
