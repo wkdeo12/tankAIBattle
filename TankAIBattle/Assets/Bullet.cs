ec6f38a09c6f06e3c0f953f61b3f21c5cd16b5cd
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-04 21:41:30 +0900

    탱크배틀 기본 셋팅
    기본셋팅완료

---
+++ TankAIBattle/Assets/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 500);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            GameObject.Destroy(collision.gameObject);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
